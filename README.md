6f27bf4a91f6bf35acbfa6047b21ffff3f261217
ShaliniSujith <neelisha@gmail.com>
2022-06-18 16:34:35 +0400

    initial files

---
+++ README.md
@@ -0,0 +1,16 @@
+A Javascript learning project where I played around with HTML canvas and manipulated using javascript.
+
+happyDancingBall.html
+happyDB.css
+happyDB.js
+
+In JS Moslty focusing on "Mouseover Element"
+
+concept covered:
+- Functions & if-else statements
+- Random 
+- Event listeners
+
+
+What the JS does
+- When you hover your mouse over a coloured ball, it gets bigger.
---
+++ happyDB.js
@@ -0,0 +1,97 @@
+console.log("Hello hasta da vista");
+
+var canvas = document.getElementById("canvasd")
+var c = canvas.getContext("2d");
+var tx = window.innerWidth;
+var ty = window.innerHeight;
+canvas.width = tx;
+canvas.height = ty;
+
+var mousex = 0;
+var mousey = 0;
+
+addEventListener("mousemove", function(){
+    mousex = event.clientX;
+    mousey = event.clientY;
+})
+
+var grav = 0.99;
+c.strokeWidth=5;
+function randomColor(){
+    return(
+        "rgba("+
+        Math.round(Math.random() * 300)+","+
+        Math.round(Math.random() * 150)+ ","+
+        Math.round(Math.random() * 240)+ ","+
+        Math.ceil(Math.random()*10)/10 + 
+        ")"
+    );
+}
+
+function Ball(){
+    this.color = randomColor();
+    this.radius = Math.random() * 25 + 20 ; 
+    this.startradius = this.radius;
+    this.x = Math.random() * (tx - this.radius * 4) + this.radius;
+    this.y = Math.random() * (ty - this.radius);
+    this.dy = Math.random() * 3
+    this.dx = Math.round(Math.random() - 1) * 4;
+    this.vel = Math.random() /6;
+    this.update = function(){
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0,4 * Math.PI);
+        c.fillStyle = this.color;
+        c.fill();
+    };
+}
+
+var bal = [];
+for(var i = 0; i <60 ; i++){
+    bal.push(new Ball());
+}
+
+function animate(){
+    if(tx != window.innerWidth || ty != window.innerHeight){
+        tx = window.innerWidth;
+        ty = window.innerHeight;
+        canvas.width = tx;
+        canvas.height = ty;
+    }
+
+    requestAnimationFrame(animate);
+    c.clearRect(0,0,tx,ty);
+    for(var i = 0; i <bal.length; i++){
+        bal[i].update();
+        bal[i].y += bal[i].dy;
+        bal[i].x += bal[i].dx;
+        if(bal[i].y + bal[i].radius >= ty){
+            bal[i].dy = -bal[i].dy * grav;
+        } else {
+            bal[i].dy += bal[i].vel;
+        }
+        if(bal[i].x + bal[i].radius > tx || bal[i].x - bal[i].radius <0){
+            bal[i].dx = -bal[i].dx;
+        }
+        if(mousex > bal[i].x -20 &&
+            mousex < bal[i].x + 20 &&
+            mousey > bal[i].y - 50 &&
+            mousey < bal[i].y + 50 &&
+            bal[i].radius < 80)
+            {
+               bal[i].radius += 10;
+            }
+            else{
+                if(bal[i].radius > bal[i].startradius){
+                    bal[i].radius += -10;
+                }
+            }
+    }
+}
+
+
+animate();
+
+setInterval(function(){
+    bal.push(new Ball());
+    bal.splice(0,1)//Remove elements from an array, insert new elemens in there place and return deleted elements
+},500);
